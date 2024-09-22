0bc8795c63611546edcf5d22f2202a1d4d2a899e
Jasjot Singh <jasjot784@gmail.com>
2024-09-22 09:52:21 +0530

    Update Leetcode_Daily_22-09-24.cpp

--- Leetcode_Daily_22-09-24.cpp
+++ Leetcode_Daily_22-09-24.cpp
@@ -1,21 +1,20 @@
 // https://leetcode.com/problems/k-th-smallest-in-lexicographical-order/description/?envType=daily-question&envId=2024-09-22
 class Solution {
 public:
-    void dfs(int temp, int n, vector<int> &ans){
-        if (temp > n){
-            return;
-        }
-
-        ans.push_back(temp);
-        dfs(temp * 10, n, ans);
-
-        if (temp % 10 != 9){
-            dfs(temp + 1, n, ans);
-        }
-    }
     int findKthNumber(int n, int k) {
-        vector<int> ans;
-        dfs(1, n, ans);
-        return ans[k-1];
+        int cur{1};
+        --k;
+        while (k) {
+            long steps = count(cur, n);
+            if (steps <= k) cur++, k -= steps;
+            else cur *= 10, --k;
+        }
+        return cur;
+    }
+    long count(long p, long n) {
+        long steps{0};
+        for (long f = p, l = p; f <= n; f *= 10, l = l * 10 + 9)
+            steps += min(n + 1, l + 1) - f;
+        return steps;
     }
 };
