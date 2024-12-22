0b767eeb8f5eefcadba8adc5c2648ffd98bef8c8
Jasjot Singh <jasjot784@gmail.com>
2024-12-22 12:47:48 +0530

    Create Leetcode_Contest_22-12-24.cpp

---
+++ Leetcode_Contest_22-12-24.cpp
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/maximum-number-of-distinct-elements-after-operations/
+int maxDistinctElements(vector<int>& nums, int k) {
+    int ans = 0;
+    sort(nums.begin(), nums.end());
+    for(int i = 0, last = INT_MIN; i < nums.size(); ++i){
+        int mn = nums[i] - k;
+        int mx = nums[i] + k;
+        if(last < mn ) { last = mn; ans++; }
+        else if(last < mx){ last = last + 1; ans++; }
+    }
+    return ans;
+}
