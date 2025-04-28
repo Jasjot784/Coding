0c3b687e3f0d11f67d58cff9c63ed6c046e84cef
Jasjot Singh <jasjot784@gmail.com>
2025-04-28 14:38:44 +0530

    Create Leetcode_28-04-25.cpp

---
+++ Leetcode_28-04-25.cpp
@@ -0,0 +1,17 @@
+// you have to remember this formula right-left+1
+// https://leetcode.com/problems/count-subarrays-with-score-less-than-k/
+class Solution {
+public:
+    using ll = long long;
+    long long countSubarrays(vector<int>& nums, ll k) {
+        ll count = 0, sum = 0, left = 0;
+        for (int right = 0; right < nums.size(); right++) {
+            sum += nums[right];
+            while (sum * (right - left + 1) >= k) {
+                sum -= nums[left++];
+            }
+            count += (right - left + 1);
+        }
+        return count;
+    }
+};
